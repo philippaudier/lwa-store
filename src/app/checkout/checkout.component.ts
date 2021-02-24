93554b34c830515eae9efe726edb7f6b6b25ca3d
philippe <philippaudier@gmail.com>
2021-02-24 06:22:15 +0100

    added even more css

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -3,12 +3,25 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CartUpdateService } from '../services/cart-update.service';
 import { ShoppingCartComponent } from '../shopping-cart/shopping-cart.component';
 
+interface Country {
+  value: string;
+  viewValue: string;
+}
+
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.scss']
 })
 export class CheckoutComponent implements OnInit {
+
+  countries: Country[] = [
+    {value: 'FRANCE', viewValue: 'FRANCE'},
+    {value: 'UNITED STATE', viewValue: 'UNITED STATE'},
+    {value: 'RUSSIA', viewValue: 'RUSSIA'},
+    {value: 'BRAZIL', viewValue: 'BRAZIL'},
+    {value: 'JAPAN', viewValue: 'JAPAN'}
+  ];
 
   cartContent: any;
   totalCost: number;
