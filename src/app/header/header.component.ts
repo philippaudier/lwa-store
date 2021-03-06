264bc42d96792d092f887e906d9e2ddea4ade340
philippe <philippaudier@gmail.com>
2021-03-06 01:06:42 +0100

    addind checkout page

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CartUpdateService } from '../services/cart-update.service';
 
 @Component({
   selector: 'app-header',
@@ -7,7 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  onCheckout = false;
 
-  ngOnInit(): void { }
+  constructor(
+    private cartUpdateService: CartUpdateService
+  ) { }
+
+  ngOnInit(): void { 
+    this.cartUpdateService.getCheckoutState().subscribe((value) => {
+      this.onCheckout = value;
+    });
+  }
 }
